b6fd49d9569bcf7a28c766b5c39771560c9b34f7
shouyamamoto <yamasyo0017@gmail.com>
2021-09-02 22:10:35 +0900

    feat: firebase-authentication github and google

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { auth, githubProvider, googleProvider } from "./firebase"
 
 const App = () => {
+  const signInGithub = async () => {
+    auth.signInWithPopup(githubProvider)
+      .then(result => {
+        console.log(result.user)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  const signInGoogle = async () => {
+    auth.signInWithPopup(googleProvider)
+      .then(result => {
+        console.log(result.user)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  const signOut = () => {
+    auth.signOut()
+    console.log("signOut")
+  }
+
   return (
     <div>
+      <button onClick={signInGithub}>Githubでログインする</button>
+      <button onClick={signInGoogle}>Googleでログインする</button>
+      <button onClick={signOut}>ログアウト</button>
     </div>
   )
 }
