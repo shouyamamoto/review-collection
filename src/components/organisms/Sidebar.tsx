f62cdaeaec501b0e9ed26ad376744a52abfb047b
shouyamamoto <yamasyo0017@gmail.com>
2021-10-15 08:22:33 +0900

    feat: twitter-share

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -1,9 +1,14 @@
 import { VFC } from "react";
 import styled from "styled-components";
-import { DEVICE } from "../../Themes/Device";
+import { TwitterShareButton, TwitterIcon } from "react-share";
+
+import { index as Typography } from "../atom/typography/index";
 import { SidebarUserProfile } from "../molecules/SidebarUserProfile";
+import { DEVICE } from "../../Themes/Device";
+import { COLOR } from "../../Themes/Color";
 
 type Props = {
+  location: string;
   uid: string;
   avatar: string;
   username: string;
@@ -14,6 +19,7 @@ type Props = {
 };
 
 export const Sidebar: VFC<Props> = ({
+  location,
   uid,
   avatar,
   username,
@@ -33,6 +39,14 @@ export const Sidebar: VFC<Props> = ({
         blogUrl={blogUrl}
         comment={comment}
       />
+      <StyledSidebarButtons>
+        <Typography size="0.8rem">この記事をシェアする</Typography>
+        <StyledButtons>
+          <TwitterShareButton url={location}>
+            <TwitterIcon size="40" round />
+          </TwitterShareButton>
+        </StyledButtons>
+      </StyledSidebarButtons>
     </StyledSidebar>
   );
 };
@@ -42,8 +56,28 @@ const StyledSidebar = styled.div`
   margin: 0 auto;
   padding: 0 14px;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 
   @media ${DEVICE.tabletL} {
     padding: 0;
   }
+`;
+
+const StyledSidebarButtons = styled.div`
+  width: 100%;
+  background-color: ${COLOR.WHITE};
+  border-radius: 10px;
+  display: grid;
+  gap: 12px;
+  padding: 20px;
+  box-sizing: border-box;
+  text-align: center;
+`;
+
+const StyledButtons = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
 `;
