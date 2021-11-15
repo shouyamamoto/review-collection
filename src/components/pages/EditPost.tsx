6513a2daac2cb5b3232471c0f72c843d02cbb3fa
shouyamamoto <yamasyo0017@gmail.com>
2021-11-15 23:55:44 +0900

    feat: customHook to editPost

--- src/components/pages/EditPost.tsx
+++ src/components/pages/EditPost.tsx
@@ -1,45 +1,12 @@
-import { VFC, useState, useEffect } from "react";
-import { useParams, Redirect } from "react-router-dom";
+import { VFC } from "react";
+import { Redirect } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import { PostArea } from "../organisms/PostArea";
-import { db } from "../../libs/firebase";
-import { useCurrentUser } from "../../hooks/useCurrentUser";
-
-type EditPostType = {
-  postId: string;
-  title: string;
-  text: string;
-  labels: string[];
-};
+import { useEditPost } from "../../hooks/EditPost/useEditPost";
 
 export const EditPost: VFC = () => {
-  const { currentUser } = useCurrentUser();
-  const { userId, postId } = useParams<{ userId: string; postId: string }>();
-  const [editPostData, setEditPostData] = useState<EditPostType>({
-    postId: postId,
-    title: "",
-    text: "",
-    labels: [],
-  });
-
-  useEffect(() => {
-    const getEditPostData = async () => {
-      const fetchPost = await db.collection("posts").doc(postId);
-      const res = fetchPost.get();
-      res.then((doc) => {
-        if (doc.exists) {
-          setEditPostData({
-            postId: postId,
-            title: doc!.data()!.title,
-            text: doc!.data()!.body,
-            labels: doc!.data()!.labels,
-          });
-        }
-      });
-    };
-    getEditPostData();
-  }, [postId]);
+  const { currentUser, userId, editPostData } = useEditPost();
 
   if (currentUser.uid !== userId) {
     return <Redirect to="/" />;
