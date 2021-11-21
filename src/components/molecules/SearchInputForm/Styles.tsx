9c0bd5177bd042c159b738ff7183fed82cc57cea
shouyamamoto <yamasyo0017@gmail.com>
2021-11-21 21:06:39 +0900

    feat: transform header

--- src/components/molecules/SearchInputForm/Styles.tsx
+++ src/components/molecules/SearchInputForm/Styles.tsx
@@ -3,7 +3,16 @@ import { FiSearch } from "react-icons/fi";
 
 import { COLOR } from "../../../Themes/Color";
 
-export const StyledSearchInputForm = styled.div`
+type OpenProps = {
+  isOpen: boolean;
+};
+
+export const StyledSearchInputForm = styled.div<OpenProps>`
+  transform: ${(props) =>
+    props.isOpen ? "translateY(0)" : "translateY(-100%)"};
+  height: ${(props) => (props.isOpen ? "auto" : "0")};
+  opacity: ${(props) => (props.isOpen ? "1" : "0")};
+  pointer-events: ${(props) => (props.isOpen ? "all" : "none")};
   display: flex;
   gap: 14px;
   align-items: center;
@@ -11,7 +20,11 @@ export const StyledSearchInputForm = styled.div`
   border: 1px solid ${COLOR.GRAY};
   padding: 0 20px;
   border-radius: 50px;
-  width: 400px;
+  width: 90%;
+  max-width: 400px;
+  margin: ${(props) => (props.isOpen ? "0 auto 40px " : "0")};
+  box-sizing: border-box;
+  transition: all 0.3s;
 `;
 
 export const StyledInput = styled.input`
